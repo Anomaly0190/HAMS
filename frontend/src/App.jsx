31d5de256f77e09bf399e3200d27153de99c3f02
sujalsubediXX <sujalsubedi541@gmail.com>
2025-04-03 21:45:11 +0545

    outlet added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 const App = () => {
   return (
-    <div>
-      <h1 className='bg-green-500 text-white'>This is sujalsubedi and git hub works</h1>
+  <>
       <Outlet />
-    </div>
+
+  </>
+
   )
 }
 
