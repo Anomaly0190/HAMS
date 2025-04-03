9d3d2d7af23f8835dd04bb0bfef7d5406759c185
sujalsubediXX <sujalsubedi541@gmail.com>
2025-04-03 18:38:23 +0545

    Changes in routes

---
+++ frontend/src/Components/User/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className='my-2 bg-black text-white'>This is headding</h1>
+    </div>
+  )
+}
+
+export default Header
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import {Toaster} from "react-hot-toast";
+
+import Header from "./Components/User/Header.jsx";
+const router=createBrowserRouter([{
+  path:"/",
+  element:<App/>,
+  children:[
+    {
+      path:"/",
+      element:<Header />
+    }
+  ]
+}])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Toaster />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
