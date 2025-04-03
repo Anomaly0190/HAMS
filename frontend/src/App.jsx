70308f7f30fde81cf937fc6ebd8bb1171f911932
sujalsubediXX <sujalsubedi541@gmail.com>
2025-04-03 18:00:29 +0545

    changed in app.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='bg-green-500 text-white'>This is sujalsubedi</h1>
+      <h1 className='bg-green-500 text-white'>This is sujalsubedi and git hub works</h1>
     </div>
   )
 }
