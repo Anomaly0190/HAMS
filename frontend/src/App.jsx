3eeb4eabd3a5a5b8bf436c4ba17012fa9436a0a1
sujalsubediXX <sujalsubedi541@gmail.com>
2025-04-03 17:43:00 +0545

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-green-500 text-white'>This is sujalsubedi</h1>
+    </div>
+  )
+}
+
+export default App
